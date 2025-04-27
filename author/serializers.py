e5cd520f49415015e2dfc162fdeb7d8dad33365a
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-27 12:19:23 +0300

    Implemented CRUD for Author model

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
+from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    first_name = serializers.CharField(max_length=64)
+    last_name = serializers.CharField(max_length=64)
+    pseudonym = serializers.CharField(
+        max_length=64, allow_blank=True, required=False, default=None
+    )
+    age = serializers.IntegerField()
+    retired = serializers.BooleanField()
+
+    class Meta:
+        model = Author
+        fields = "__all__"
+        read_only_fields = ("id",)
