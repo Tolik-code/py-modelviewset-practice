7238015dbdb0b06201609cc674529214601d8b10
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-27 12:25:45 +0300

    Update serializers.py

--- author/serializers.py
+++ author/serializers.py
@@ -3,14 +3,6 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(max_length=64)
-    last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(
-        max_length=64, allow_blank=True, required=False, default=None
-    )
-    age = serializers.IntegerField()
-    retired = serializers.BooleanField()
-
     class Meta:
         model = Author
         fields = "__all__"
